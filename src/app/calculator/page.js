d62ba9566b04fcda33c310e8afef375388dae253
Shi Heng Wong <shiheng.wong@transferwise.com>
2024-01-25 18:35:19 +0800

    calaculator basic - 2

---
+++ src/app/calculator/page.js
@@ -0,0 +1,10 @@
+import ResponsiveAppBar from "@/components/ResponsiveAppBar";
+
+export default function CalculatorPage() {
+  return (
+    <>
+    <ResponsiveAppBar></ResponsiveAppBar>
+    This is the calculator page.
+    </>
+  );
+}
