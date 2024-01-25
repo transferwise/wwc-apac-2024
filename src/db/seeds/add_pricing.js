cfcf60e9924d24c2cebd00ca9e6367e4da510780
Saloni Kaur <saloni.kaur@wise.com>
2024-01-25 19:41:33 +0800

    add more pricing

--- src/db/seeds/add_pricing.js
+++ src/db/seeds/add_pricing.js
@@ -8,6 +8,8 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex(tableName).del()
   await knex(tableName).insert([
-    {id: 1, transferId: 123, fee: 0.01486, conversionRate: 0.74}
+    {id: 1, transferId: 1, fee: 0.01486, conversionRate: 0.74},
+    {id: 2, transferId: 2, fee: 0.01486, conversionRate: 0.74},
+    {id: 3, transferId: 3, fee: 0.01486, conversionRate: 0.74},
   ]);
 };
