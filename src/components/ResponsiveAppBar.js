03299ccc3c4fb489097fabcf209a2dcd40af7c7f
Mabel Lee <mabel.lee@transferwise.com>
2024-02-25 16:03:40 +0800

    cleanup

--- src/components/ResponsiveAppBar.js
+++ src/components/ResponsiveAppBar.js
@@ -100,7 +100,7 @@ function ResponsiveAppBar() {
             variant="h5"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/"
             sx={{
               mr: 2,
               display: { xs: 'flex', md: 'none' },
